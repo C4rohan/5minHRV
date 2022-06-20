27752eb2e3ee9736a7c5830b74a39b2694ff95e8
Rohan Salvi <62870990+C4rohan@users.noreply.github.com>
2022-06-20 11:14:27 -0500

    Update 5mindata.py

--- 5mindata.py
+++ 5mindata.py
@@ -6,18 +6,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import heartpy as hp
 import csv 
-
+import BinarySearch
 
 path = os.getcwd()
 # field names 
-fields = ['Patient','5 Time','Day','BPM', 'ibi', 'sdnn', 'rmssd','hr_mad','sd1','sd2','sd1/sd2',] 
+fields = ['Patient','5Min Time','Start time','End Time','Day','Coverage','BPM', 'ibi', 'sdnn', 'rmssd','hr_mad','sd1','sd2','sd1/sd2',] 
 
 filename5 ="5MinMeasure.csv"
 with open(filename5,'a') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(fields)
 
-directory = 'D:/Rohan/RA/new data/'
+directory = 'D:/Rohan/RA/waveform/'
 
 
 #numOfDirs = len(next(os.walk(directory))[1])
@@ -43,65 +43,116 @@ for x in sub_folders:
     for singleFile in dir_list:
         
       if os.path.getsize(tempDir + singleFile)!=0:  
-                    print(tempDir + singleFile + "\n")
-                    data = hp.get_data(tempDir + singleFile)
-                    sample_rate = 240
-                    chunk_size = 72000 
-                    num_chunks = len(data) // chunk_size + 1
-                    #print(num_chunks)
-                    first=1
-                    second=chunk_size
+            print(tempDir + singleFile + " --end\n")
 
-                    if num_chunks == 1:
+            #samplefile = 'D:\Rohan\RA\waveform\p2_Patient\ECG\ECG_1.csv'
+            timedata = hp.get_data(tempDir + singleFile, column_name = 'Time2')
+            data = hp.get_data(tempDir + singleFile, column_name = 'Value')
+            
+            print("Test")
+            sample_rate = 240
+            chunk_size = 72000 
+            num_chunks = len(data) // chunk_size + 1
+            #print(num_chunks)
+            first=1
+            second=chunk_size
+
+            if num_chunks == 1:
+                second=len(data)
+                print(num_chunks)
+
+            for i in range(num_chunks * 1000):
+                print(i)
+                if i >= 1:
+                    first = second + 1
+                    second= second + chunk_size
+                    if second >= len(data):
                         second=len(data)
-                        print(num_chunks)
-                    for i in range(num_chunks):
-                        print(i)
-                        if i >= 1:
-                            first = second + 1
-                            second= second + chunk_size
-                            if second >= len(data):
-                                second=len(data) 
-                        try:
-                                filtered = hp.filter_signal(data[first:second], cutoff = 0.05, sample_rate = sample_rate, filtertype='notch')
-                                wd, m = hp.process(filtered, sample_rate)
-                                #display computed measures
-                                rows= [tempDir,i,singleFile,m['bpm'], m['ibi'], m['sdnn'], m['rmssd'],m['hr_mad'],m['sd1'],m['sd2'],m['sd1/sd2']]
-                                with open(filename5 ,'a') as csvfile: 
-                                # creating a csv writer object 
-                                    csvwriter = csv.writer(csvfile) 
-                                    # writing the data rows 
-                                    csvwriter.writerow(rows)
-                                    print("File _Write")
-                        except:
-                                print("Failed | An exception occurred")
-                                try:
-                                    print("Trying smoothning")
-                                    smoothed = hp.smooth_signal(data[first:second], sample_rate = sample_rate, window_length=4, polyorder=2)
-                                    print("Smoothed")
-                                    wd, m = hp.process(smoothed, sample_rate)
-                                    #display computed measures
-                                    rows= [tempDir,i,singleFile,m['bpm'], m['ibi'], m['sdnn'], m['rmssd'],m['hr_mad'],m['sd1'],m['sd2'],m['sd1/sd2']]
-                                    #print(rows)
+                    try:
+                        if timedata[second] - timedata[first] < 300:
+                            second = second + 240
+                    except:
+                        print()
+                    az=(timedata[first:second])
+                    temp_az = np.rint(az)
+                    az_unique = np.unique(temp_az)
+                    
+                    if len(az_unique) == 0:
+                        break
 
-                                    # writing to csv file 
-                                    with open(filename5,'a') as csvfile: 
-                                    # creating a csv writer object 
-                                        csvwriter = csv.writer(csvfile) 
-                                        csvwriter.writerow(rows)
-                                    #print(rows)
-                                    print("File _Written")
-                                except:
-                                    print("Failed at Reading")
-                                    rows= [tempDir,i,singleFile,'NA','NA','NA','NA','NA','NA','NA','NA']            
-                                    with open(filename5,'a') as csvfile: 
-                                    # creating a csv writer object 
-                                        csvwriter = csv.writer(csvfile) 
-                                        csvwriter.writerow(rows)
-    else:
-                        print("File size 0")
-                        rows= [tempDir,singleFile,'NA','NA','NA','NA','NA','NA','NA','NA']            
-                        with open(filename5,'a') as csvfile: 
-                    #creating a csv writer object 
+                    first_val = az_unique[0]
+                    for j in range(1,len(az_unique)):
+                        #print('val i ' +str(az_unique[i]) + 'val i-1 '+str(az_unique[i-1]))
+                        if(az_unique[j] - az_unique[j-1] > 10):
+                            #second = ((i-1) * 240) + first
+
+
+                            # Function call
+                            result = BinarySearch.binary_search(temp_az, 0, len(temp_az)-1, az_unique[j-1])
+                            if result != -1:
+                                print("Element is present at index", str(result))
+                                second = first + result
+                                print("Actual value " + str(timedata[second-1]) + " " + str(timedata[second]) + " " + str(timedata[second+1]))
+                            else:
+                                print("Element is not present in array | element--> " + str(az_unique[j-1]))
+                            break
+
+                    
+                    print("first -- " + str(first))
+                    print("second -- " + str(second))
+                try:
+                        filtered = hp.filter_signal(data[first:second], cutoff = 0.05, sample_rate = sample_rate, filtertype='notch')
+                        per= round((len(filtered)/chunk_size)*100, 2)
+                        wd, m = hp.process(filtered, sample_rate)
+                        #m=timedata[first]
+        
+                        F=timedata[first]
+                        S=timedata[second]
+
+                        print("timedata",F)
+                        print("timedata",S)
+
+                        #display computed measures
+                        rows= [tempDir,i,F,S,singleFile,per,m['bpm'], m['ibi'], m['sdnn'], m['rmssd'],m['hr_mad'],m['sd1'],m['sd2'],m['sd1/sd2']]
+                        with open(filename5 ,'a') as csvfile: 
+                        # creating a csv writer object 
                             csvwriter = csv.writer(csvfile) 
+                            # writing the data rows 
                             csvwriter.writerow(rows)
+                            print("File _Write")
+                except:
+                        print("Failed | An exception occurred")
+                        try:
+                            print("Trying smoothning")
+                            smoothed = hp.smooth_signal(data[first:second], sample_rate = sample_rate, window_length=4, polyorder=2)
+                            print("Smoothed")
+                            per1= (len(smoothed)/len(timedata))*100
+                            wd, m = hp.process(smoothed, sample_rate)
+                            F=timedata[first]
+                            S=timedata[second]
+                            #display computed measures
+                            rows= [tempDir,i,F,S,singleFile,per1,m['bpm'], m['ibi'], m['sdnn'], m['rmssd'],m['hr_mad'],m['sd1'],m['sd2'],m['sd1/sd2']]
+                            #print(rows)
+
+                            # writing to csv file 
+                            with open(filename5,'a') as csvfile: 
+                            # creating a csv writer object 
+                                csvwriter = csv.writer(csvfile) 
+                                csvwriter.writerow(rows)
+                            #print(rows)
+                            print("File _Written")
+                        except:
+                            print("Failed at Reading")
+                            per1=(len(data[first:second])/chunk_size)
+                            rows= [tempDir,i,'NA','NA',singleFile,per1,'NA','NA','NA','NA','NA','NA','NA','NA']            
+                            with open(filename5,'a') as csvfile: 
+                            # creating a csv writer object 
+                                csvwriter = csv.writer(csvfile) 
+                                csvwriter.writerow(rows)
+      else:
+            print("File size 0")
+            rows= [tempDir,i,'NA','NA',singleFile,'NA','NA','NA','NA','NA','NA','NA','NA','NA']            
+            with open(filename5,'a') as csvfile: 
+        #creating a csv writer object 
+                csvwriter = csv.writer(csvfile) 
+                csvwriter.writerow(rows)
